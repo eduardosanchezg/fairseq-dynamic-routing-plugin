ac076c7ffc2467dda6d3583e90426b7790355cb7
eduardo <eduardosanchezg37@gmail.com>
2021-10-14 17:45:00 +0200

    -

--- examples/dynamic_routing/dynamic_routing_src/modules/capsnet_transformer_layer.py
+++ examples/dynamic_routing/dynamic_routing_src/modules/capsnet_transformer_layer.py
@@ -98,7 +98,7 @@ class CapsuleSubLayer(nn.Module):
         # Matrix product of 2 tensors with shape: [128, 1152, 10, 16, 8] x [128, 1152, 10, 8, 1]
         # u_hat shape: [128, 1152, 10, 16, 1]
         batch_weight = batch_weight.cuda()
-        u_hat = torch.matmul(batch_weight, x)
+        u_hat = batch_weight @ x
 
 
         # All the routing logits (b_ij in the paper) are initialized to zero.
