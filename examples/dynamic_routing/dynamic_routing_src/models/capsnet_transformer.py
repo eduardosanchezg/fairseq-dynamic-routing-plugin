0e5ac813f015e9c7b580033070dfd960c4158466
eduardo <eduardosanchezg37@gmail.com>
2021-10-14 02:01:01 +0200

    first commit

---
+++ examples/dynamic_routing/dynamic_routing_src/models/capsnet_transformer.py
@@ -0,0 +1,45 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch.nn as nn
+from fairseq.models import register_model, register_model_architecture
+from fairseq.models.transformer import (
+    TransformerDecoder,
+    TransformerEncoder,
+    base_architecture,
+)
+
+from fairseq.models.transformer import TransformerModel
+from ..modules.capsnet_transformer_layer import CapsNetTransformerEncoderLayer
+
+
+@register_model("capsnet_transformer")
+class CapsNetTransformerModel(TransformerModel):
+    """TODO: A variant of Transformer as is in "xxx"
+    (https://arxiv.org/abs/2009.13102).
+    """
+
+    @classmethod
+    def build_encoder(cls, args, src_dict, embed_tokens):
+        return CapsNetTransformerEncoder(args, src_dict, embed_tokens)
+
+
+class CapsNetTransformerEncoder(TransformerEncoder):
+    """Residual drop (https://) implemented in
+    TransformerEncoder.
+    """
+
+    def __init__(self, args, dictionary, embed_tokens):
+        super().__init__(args, dictionary, embed_tokens)
+
+        self.layers[0] = nn.CapsNetTransformerEncoderLayer(args, dictionary, embed_tokens)
+
+
+
+@register_model_architecture(
+    "capsnet_transformer", "capsnet_transformer"
+)
+def capsnet_transformer_architecture(args):
+    base_architecture(args)
---
+++ examples/dynamic_routing/dynamic_routing_src/modules/capsnet_transformer_layer.py
@@ -0,0 +1,149 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import Dict, List, Optional
+
+import torch
+import torch.nn as nn
+from fairseq import utils
+from fairseq.modules import TransformerEncoderLayer
+from torch import Tensor
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+import numpy as np
+
+BATCH_SIZE = 100
+NUM_CLASSES = 10
+NUM_EPOCHS = 500
+NUM_ROUTING_ITERATIONS = 3
+
+from torch.autograd import Variable
+from torch.optim import Adam
+from torchnet.engine import Engine
+from torchnet.logger import VisdomPlotLogger, VisdomLogger
+from tqdm import tqdm
+import torchnet as tnt
+
+class CapsuleSubLayer(nn.Module):
+    def __init__(self, num_capsules, num_route_nodes, in_channels, out_channels, kernel_size=None, stride=None,
+                 num_iterations=NUM_ROUTING_ITERATIONS):
+        super(CapsuleSubLayer, self).__init__()
+
+        self.num_route_nodes = num_route_nodes
+        self.num_iterations = num_iterations
+
+        self.num_capsules = num_capsules
+
+        if num_route_nodes != -1:
+            self.route_weights = nn.Parameter(torch.randn(num_capsules, num_route_nodes, in_channels, out_channels))
+        else:
+            self.capsules = nn.ModuleList(
+                [nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=0) for _ in
+                 range(num_capsules)])
+
+    def squash(self, tensor, dim=-1):
+        squared_norm = (tensor ** 2).sum(dim=dim, keepdim=True)
+        scale = squared_norm / (1 + squared_norm)
+        return scale * tensor / torch.sqrt(squared_norm)
+
+    def forward(self, x):
+        if self.num_route_nodes != -1:
+            priors = x[None, :, :, None, :] @ self.route_weights[:, None, :, :, :]
+
+            logits = Variable(torch.zeros(*priors.size())).cuda()
+            for i in range(self.num_iterations):
+                probs = F.softmax(logits, dim=2)
+                outputs = self.squash((probs * priors).sum(dim=2, keepdim=True))
+
+                if i != self.num_iterations - 1:
+                    delta_logits = (priors * outputs).sum(dim=-1, keepdim=True)
+                    logits = logits + delta_logits
+        else:
+            outputs = [capsule(x).view(x.size(0), -1, 1) for capsule in self.capsules]
+            outputs = torch.cat(outputs, dim=-1)
+            outputs = self.squash(outputs)
+
+        return outputs
+
+class CapsNetTransformerEncoderLayer(TransformerEncoderLayer):
+    """Encoder layer block. TODO: comment
+
+    In the original paper each operation (multi-head attention or FFN) is
+    postprocessed with: `dropout -> add residual -> layernorm`. In the
+    tensor2tensor code they suggest that learning is more robust when
+    preprocessing each layer with layernorm and postprocessing with:
+    `dropout -> add residual`. We default to the approach in the paper, but the
+    tensor2tensor approach can be enabled by setting
+    *args.encoder_normalize_before* to ``True``.
+
+    Args:
+        args (argparse.Namespace): parsed command-line arguments
+    """
+
+    def __init__(self, args, drop_residual_after_att=False):
+        super().__init__(args)
+        #print('****************************', drop_residual_after_att)
+        #self.drop_residual_after_att = drop_residual_after_att
+
+    def forward(self, x, encoder_padding_mask: Optional[Tensor], attn_mask: Optional[Tensor] = None):
+        """
+        Args:
+            x (Tensor): input to the layer of shape `(seq_len, batch, embed_dim)`
+            encoder_padding_mask (ByteTensor): binary ByteTensor of shape
+                `(batch, seq_len)` where padding elements are indicated by ``1``.
+            attn_mask (ByteTensor): binary tensor of shape `(tgt_len, src_len)`,
+                where `tgt_len` is the length of output and `src_len` is the
+                length of input, though here both are equal to `seq_len`.
+                `attn_mask[tgt_i, src_j] = 1` means that when calculating the
+                embedding for `tgt_i`, we exclude (mask out) `src_j`. This is
+                useful for strided self-attention.
+
+        Returns:
+            encoded output of shape `(seq_len, batch, embed_dim)`
+        """
+        # anything in original attn_mask = 1, becomes -1e8
+        # anything in original attn_mask = 0, becomes 0
+        # Note that we cannot use -inf here, because at some edge cases,
+        # the attention weight (before softmax) for some padded element in query
+        # will become -inf, which results in NaN in model parameters
+        #if attn_mask is not None:
+        #    attn_mask = attn_mask.masked_fill(attn_mask.to(torch.bool), -1e8)
+
+        residual = x
+        if self.normalize_before:
+            x = self.self_attn_layer_norm(x)
+        # x, _ = self.self_attn(
+        #     query=x,
+        #     key=x,
+        #     value=x,
+        #     key_padding_mask=encoder_padding_mask,
+        #     need_weights=False,
+        #     attn_mask=attn_mask,
+        # )
+
+        capsnet = CapsuleSubLayer(num_capsules=8, num_route_nodes=-1, in_channels=256, out_channels=32,
+                                             kernel_size=9, stride=2)
+
+        x = capsnet.forward(x)
+
+        x = self.dropout_module(x)
+
+        x = self.residual_connection(x, residual)
+        if not self.normalize_before:
+            x = self.self_attn_layer_norm(x)
+
+        residual = x
+        if self.normalize_before:
+            x = self.final_layer_norm(x)
+        x = self.activation_fn(self.fc1(x))
+        x = self.activation_dropout_module(x)
+        x = self.fc2(x)
+        x = self.dropout_module(x)
+        x = self.residual_connection(x, residual)
+        if not self.normalize_before:
+            x = self.final_layer_norm(x)
+        return x
