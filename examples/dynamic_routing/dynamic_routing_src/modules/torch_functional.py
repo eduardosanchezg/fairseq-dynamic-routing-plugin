df368a7040abe9b530432c6835b9eaa520eb5ff7
eduardo <eduardosanchezg37@gmail.com>
2021-10-19 20:03:30 +0200

    -

--- examples/dynamic_routing/dynamic_routing_src/modules/torch_functional.py
+++ examples/dynamic_routing/dynamic_routing_src/modules/torch_functional.py
@@ -4416,7 +4416,7 @@ def multi_head_attention_forward(
 
     #attn_output = attn_output.transpose(0, 1).contiguous().view(tgt_len, bsz, embed_dim)
 
-    l = torch.nn.Linear(35840, 1376256)
+    l = torch.nn.Linear(160, 6144)
     linear_output = l.forward(capsule_vectors)
     linear_output = linear_output.transpose(0, 1).contiguous().view(tgt_len, bsz, embed_dim)
     attn_output = linear(linear_output, out_proj_weight, out_proj_bias)
