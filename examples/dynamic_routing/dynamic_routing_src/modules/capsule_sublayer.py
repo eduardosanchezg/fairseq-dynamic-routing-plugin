adffaa4c32a6172e70dd8a10232b6815625c5c9a
eduardo <eduardosanchezg37@gmail.com>
2021-10-20 15:05:53 +0200

    -

--- examples/dynamic_routing/dynamic_routing_src/modules/capsule_sublayer.py
+++ examples/dynamic_routing/dynamic_routing_src/modules/capsule_sublayer.py
@@ -44,7 +44,8 @@ class CapsuleSubLayer(nn.Module):
             # weight shape:
             # [1 x primary_unit_size x num_classes x output_unit_size x num_primary_unit]
             # == [1 x 1152 x 10 x 16 x 8]
-            self.weight = nn.Parameter(torch.randn(1 ,unit_size, num_unit,in_channel , in_unit))
+            #self.weight = nn.Parameter(torch.randn(1 ,unit_size, num_unit,in_channel , in_unit))
+            self.weight = nn.Parameter(torch.randn(1 ,unit_size, num_unit,in_channel , num_unit))
         else:
             """
             
