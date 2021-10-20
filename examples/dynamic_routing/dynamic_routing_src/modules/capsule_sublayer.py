e077cdb5c8b44b513424f9cb268e670c3b88ae40
eduardo <eduardosanchezg37@gmail.com>
2021-10-20 02:44:26 +0200

    -

--- examples/dynamic_routing/dynamic_routing_src/modules/capsule_sublayer.py
+++ examples/dynamic_routing/dynamic_routing_src/modules/capsule_sublayer.py
@@ -104,7 +104,7 @@ class CapsuleSubLayer(nn.Module):
         # print(batch_weight.size())
         # print(x.size())
 
-        u_hat = torch.matmul(batch_weight.cuda(), x)
+        u_hat = torch.matmul(batch_weight.cuda().half(), x)
 
         # print("|||||||||||||||||||||||||||||||||| U HAT ||||||||||||||||||||||||||||||||||")
         # print(u_hat.size())
