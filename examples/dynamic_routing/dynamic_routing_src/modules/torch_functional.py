fe3b804f99b3312af29fd07b7dc64e792b91076e
eduardo <eduardosanchezg37@gmail.com>
2021-10-21 18:46:03 +0200

    -

--- examples/dynamic_routing/dynamic_routing_src/modules/torch_functional.py
+++ examples/dynamic_routing/dynamic_routing_src/modules/torch_functional.py
@@ -4443,9 +4443,9 @@ def multi_head_attention_forward(
     #capsule_vectors = capsule_vectors.transpose(0, 1).contiguous().view(tgt_len, bsz, embed_dim)
     attn_output = linear(capsule_vectors, capsule_proj_weight, capsule_proj_bias)
     attn_output = attn_output.transpose(0,1)
-    print("||||||| linear_output |||||")
-    print(attn_output.size())
-    print("|||||||||||||||||||||1")
+    # print("||||||| linear_output |||||")
+    # print(attn_output.size())
+    # print("|||||||||||||||||||||1")
 
     if need_weights:
         # average attention weights over heads
