c7414ac4a603aae5ec0c2480f095acfb7ee90322
eduardo <eduardosanchezg37@gmail.com>
2021-10-21 21:24:08 +0200

    -

--- examples/dynamic_routing/dynamic_routing_src/modules/capsnet_transformer_layer.py
+++ examples/dynamic_routing/dynamic_routing_src/modules/capsnet_transformer_layer.py
@@ -42,7 +42,7 @@ class CapsNetTransformerEncoderLayer(TransformerEncoderLayer):
         super().__init__(args)
         self.capsule_proj_weight = None#Parameter(torch.randn(512, 16))
         self.capsule_proj_bias = None#Parameter(torch.randn(512))    #emb_size
-        self.dynamic_routing_weight = nn.Parameter(torch.randn(1 ,512, 128, 16 , 128))
+        self.dynamic_routing_weight = None #nn.Parameter(torch.randn(1 ,512, 128, 16 , 128))
 
         # print("||||||||||||||TENSOR CHECK ||||||||||")
         # print(self.dynamic_routing_weight.size())
