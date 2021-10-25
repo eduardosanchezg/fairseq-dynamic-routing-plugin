128355639100547a3b756677706a6bec063e22ba
eduardo <eduardosanchezg37@gmail.com>
2021-10-25 02:27:06 +0200

    -

--- examples/dynamic_routing/dynamic_routing_src/modules/modified_multihead_attention.py
+++ examples/dynamic_routing/dynamic_routing_src/modules/modified_multihead_attention.py
@@ -82,8 +82,10 @@ class ModifiedMultiheadAttention(nn.Module):
             nn.Linear(512, 16, bias=bias), q_noise, qn_block_size
         )
 
-        self.capsule_proj_weight = Parameter(torch.randn(512, 16))
-        self.capsule_proj_bias = Parameter(torch.randn(512))  # emb_size
+        # self.capsule_proj_weight = Parameter(torch.randn(512, 16))
+        # self.capsule_proj_bias = Parameter(torch.randn(512))  # emb_size
+        self.capsule_proj_weight = Parameter(torch.randn(1, 1))
+        self.capsule_proj_bias = Parameter(torch.randn(1))  # emb_size
         #self.dynamic_routing_weight = nn.Parameter(torch.randn(1, 512, 128, 16, 128))
         self.dynamic_routing_weight = nn.Parameter(torch.randn(1, 1, 1, 1, 1))
 
