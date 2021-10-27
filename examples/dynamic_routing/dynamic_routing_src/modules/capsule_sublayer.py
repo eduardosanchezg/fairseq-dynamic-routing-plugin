ef325cfcd4b4baccc6fae14ddb7ab6574835ba5f
eduardo <eduardosanchezg37@gmail.com>
2021-10-27 14:37:50 +0200

    added division by 10

--- examples/dynamic_routing/dynamic_routing_src/modules/capsule_sublayer.py
+++ examples/dynamic_routing/dynamic_routing_src/modules/capsule_sublayer.py
@@ -98,8 +98,8 @@ class CapsuleSubLayer(nn.Module):
             # print(stacked_u_i.size())
             u_hat = torch.matmul(batch_weight, stacked_u_i.transpose(1,3))
 
-            print("|||||||||||||||||||||||||||||||||| U HAT ||||||||||||||||||||||||||||||||||")
-            print(u_hat.abs().mean())
+            # print("|||||||||||||||||||||||||||||||||| U HAT ||||||||||||||||||||||||||||||||||")
+            # print(u_hat.abs().mean())
 
             # All the routing logits (b_ij in the paper) are initialized to zero.
             # self.in_channel = primary_unit_size = 32 * 6 * 6 = 1152
@@ -185,11 +185,12 @@ class CapsuleSubLayer(nn.Module):
 
 
                 u_vj1 = torch.matmul(u_hat, v_j1.half()).mean(dim=3, keepdim=True).mean(dim=0, keepdim=True)
-
-                print("||||u_vj1|||")
-                print(u_vj1.abs().mean())
-                print(iteration)
-                print("||||||||||||||||||||")
+                if u_vj1.abs().mean() > 10:
+                    u_vj1 = u_vj1 / 10
+                # print("||||u_vj1|||")
+                # print(u_vj1.abs().mean())
+                # print(iteration)
+                # print("||||||||||||||||||||")
 
                 # Update routing (b_ij) by adding the agreement to the initial logit.
 
