0ffbef3d624c8e39b32f500e50f56d7da80d8eea
eduardo <eduardosanchezg37@gmail.com>
2021-10-31 14:31:20 +0100

    -

--- examples/dynamic_routing/dynamic_routing_src/modules/capsule_sublayer.py
+++ examples/dynamic_routing/dynamic_routing_src/modules/capsule_sublayer.py
@@ -57,13 +57,13 @@ class CapsuleSubLayer(nn.Module):
 
         u = x.transpose(0,2).contiguous().view(joint_batch,num_in,in_dim) # [joint_batch, num_in, in_dim]
 
-        u[u==0.0] = np.random.uniform(-0.5, 0.5)
+        u[u==0.0] = np.random.uniform(-1, 1)
 
-        print("||||||||||||||||||U||||||||||||||||||||||||")
-        try:
-            print(u[4060,:,:])
-        except:
-            print("INDEX NOT VALID")
+        # print("||||||||||||||||||U||||||||||||||||||||||||")
+        # try:
+        #     print(u[4060,:,:])
+        # except:
+        #     print("INDEX NOT VALID")
 
         stacked_u = torch.stack([u] * num_out, dim=2) # [joint_batch, num_in, num_out, in_dim]
 
